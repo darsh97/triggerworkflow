93c8072bdc75857bef801a448bad55c3e6719395
Darshan <aravinddarshan13897@outlook.com>
2022-03-30 22:06:43 +0530

    updated s3 check code

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import boto3
 from typing import final
 import json
 import requests
+
 ############## CONSTANTS #################
 
 s3 = boto3.resource('s3')
@@ -15,16 +16,39 @@ REPO: final = "repository"
 API: final = "api"
 TRIGGER_REQUEST: final = "trigger"
 EVENT_TYPE: final = "product1_inference"
+PROCESSED_FILES_FOLDER: str = 'processed_files/'
+
 
 ############## HELPERS #################
 
 def check_object_exists(prefix):
-    objects = [*BUCKET.objects.filter(Prefix=prefix)]
-    return bool(len(objects))
+    all_objects = [*BUCKET.objects.filter(Prefix=prefix)]
+
+    previously_processed_files = {pf.key.split('/')[-1] for pf in BUCKET.objects.filter(Prefix='processed_files/')}
+
+    files_to_be_processed = []
+
+    for ob in all_objects:
+        if ob.key not in previously_processed_files:
+            files_to_be_processed.append(ob.key)
+
+    for ob in all_objects:
+        current_object_key = ob.key
+
+        copy_source = {
+            'Bucket': 'codebucket234',
+            'Key': current_object_key
+        }
+
+        s3.meta.client.copy(copy_source, "codebucket234", f"{PROCESSED_FILES_FOLDER}{current_object_key}")
+
+    return bool(len(files_to_be_processed))
+
 
 def get_access_token():
     access_token = os.environ.get(ACCESS_TOKEN)
     return access_token
+
 
 def get_config_file_details(config_file_path: str):
     with open(config_file_path, "r") as f:
@@ -32,13 +56,11 @@ def get_config_file_details(config_file_path: str):
     return config
 
 
-################ MAIN ######################
-
 object_exists: bool = check_object_exists(PREFIX)
 
 if object_exists:
     access_token: str = get_access_token()
-    config  = get_config_file_details(CONFIG_FILE)
+    config = get_config_file_details(CONFIG_FILE)
     owner: str = config[OWNER]
     repo: str = config[REPO]
     trigger_req_api: str = config[API][TRIGGER_REQUEST]
@@ -50,7 +72,7 @@ if object_exists:
 
     trigger_request: str = trigger_req_api.format(owner=owner, repo=repo)
     event_data = {"event_type": EVENT_TYPE}
-    
+
     response = requests.post(
         url=trigger_request,
         headers=headers,
@@ -58,5 +80,3 @@ if object_exists:
     )
 
     print(response.ok, response.content)
-    
-    
